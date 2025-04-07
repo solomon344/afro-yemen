0e6fa29026fbf6b45fa968031fc067acaf176a0e
solomon344 <willamssolomon672@gmail.com>
2025-04-07 15:39:14 +0000

    edited the core values

--- src/components/coreValues.jsx
+++ src/components/coreValues.jsx
@@ -1,7 +1,25 @@
 'use client'
 import { HeartHandshake, Share2Icon,Heart } from "lucide-react"
 
+
 const CoreValues = () => {
+  const core_values = [
+    {
+      name:"Honesty",
+      icon:<HeartHandshake size={'2rem'}  className=" text-black" />,
+      description:"The well being of our beneficiaries is our priority, we invest every single penny we get from donations."
+    },
+    {
+      name:"Sharing",
+      icon:<Share2Icon size={'2rem'}  className="text-black" />,
+      description:"We believe in giving back to the community. We have no exception to who benefits from us."
+    },
+    {
+      name:"Caring",
+      icon:<Heart size={'2rem'}  className="text-black" />,
+      description:"We strive to make every life count. We are there for each and every one of you."
+    }
+  ]
   return (
     <div className='flex justify-center core bg-black text-gray-300 p-10'>
         <div className='flex flex-col gap-4 lg:w-3/4'>
@@ -9,43 +27,23 @@ const CoreValues = () => {
 
         <div className='grid grid-cols-3 gap-3 lg:p-6 pl-0'>
             
-        <div className="flex items-center justify-center gap-3">
+        {
+          core_values.map((item,index) => (
+          <div className="flex items-center justify-center gap-3">
           <div className="bg-gray-100 rounded-full p-2">
-          <HeartHandshake size={'2rem'}  className=" text-black" />
+          {item.icon}
           </div>
          <div className="flex flex-col gap-1">
-         <p className="font-bold uppercase underline underline-offset-4 decoration-yellow-300 decoration-2">Honesty</p>
-         <p className="line-clamp-2 text-sm text-gray-400">
-          The well being of our beneficiaries is our priority, we invest every single penny we get from donations.
+         <p className="font-bold uppercase underline underline-offset-4 decoration-yellow-300 decoration-2">{item.name}</p>
+         <p title={item.description} className="line-clamp-2 text-sm text-gray-400">
+          {item.description}
          </p>
          </div>
         </div>
+          ))
+        }
 
-        <div className="flex items-center justify-center gap-3">
-          <div className="bg-gray-100 rounded-full p-2">
-          <Share2Icon size={'2rem'}  className="text-black" />
-          </div>
-         <div className="flex flex-col gap-1">
-         <p className="font-bold uppercase underline underline-offset-4 decoration-yellow-300 decoration-2">Sharing</p>
-         <p className="line-clamp-2 text-sm text-gray-400">
-          We believe in giving back to the community. We have no exception to who benefits from us.
-         </p>
-         </div>
-        </div>
-
-
-        <div className="flex items-center justify-center gap-3">
-          <div className="bg-gray-100 rounded-full p-2">
-          <Heart size={'2rem'}  className="text-black" />
-          </div>
-         <div className="flex flex-col gap-1">
-         <p className="font-bold uppercase underline underline-offset-4 decoration-yellow-300 decoration-2">Caring</p>
-         <p className="line-clamp-2 text-sm text-gray-400">
-          We strive to make every life count. We are there for each and every one of you.
-         </p>
-         </div>
-        </div>
-
+        
         </div>
 
         </div>
